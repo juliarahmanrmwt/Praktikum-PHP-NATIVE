11fd2fb7950b81dc8dc9950debdadd13029117e3
juliarahmanurmawati <juliarahman@student.uns.ac.id>
2026-04-25 16:39:34 +0700

    up

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,6 +1,30 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "db_petani");
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+// Data dari TiDB Cloud
+$host = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com';
+$port = 4000;
+$user = 'JVqtrmn5CnmfKr7.root';
+$pass = 'IvzAAFxyiAJ3L1tO';
+$db   = 'db_petani';
+
+// Inisialisasi mysqli
+$koneksi = mysqli_init();
+
+// Menambahkan pengaturan SSL (Wajib untuk TiDB Serverless)
+mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
+
+// Melakukan koneksi
+$real_connect = mysqli_real_connect(
+    $koneksi, 
+    $host, 
+    $user, 
+    $pass, 
+    $db, 
+    $port, 
+    NULL, 
+    MYSQLI_CLIENT_SSL
+);
+
+if (!$real_connect) {
+    die("Koneksi ke TiDB Cloud gagal: " . mysqli_connect_error());
 }
 ?>
