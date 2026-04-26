0879d076d129c089f8132462f5ecc2b016d9c9b3
juliarahmanurmawati <juliarahman@student.uns.ac.id>
2026-04-26 11:29:58 +0700

    up

--- api/register.php
+++ api/register.php
@@ -2,16 +2,16 @@
 include 'koneksi.php';
 
 if (isset($_POST['register'])) {
-    $nama = mysqli_real_escape_string($conn, $_POST['nama']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $nama = mysqli_real_escape_string($koneksi, $_POST['nama']);
+    $email = mysqli_real_escape_string($koneksi, $_POST['email']);
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    $cek_email = mysqli_query($conn, "SELECT email FROM users WHERE email='$email'");
+    $cek_email = mysqli_query($koneksi, "SELECT email FROM users WHERE email='$email'");
     if (mysqli_num_rows($cek_email) > 0) {
         echo "<script>alert('Email sudah terdaftar!');</script>";
     } else {
         $query = "INSERT INTO users (nama_lengkap, email, password) VALUES ('$nama', '$email', '$password')";
-        if (mysqli_query($conn, $query)) {
+        if (mysqli_query($koneksi, $query)) {
             echo "<script>alert('Berhasil daftar! Silakan login.'); window.location='login.php';</script>";
         }
     }
